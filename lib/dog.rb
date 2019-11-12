2fdbb4a910f081cae9c3e932fbc629d18e882b6c
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-12 01:30:05 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+ 
+attr_accessor :name, :breed
+attr_reader :id
+ 
+  def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql =  <<-SQL
+      DROP TABLE IF EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+      DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+  
+  def self.new_from_db(row)
+    new_dog = self.new  
+    new_dog.id = row[0]
+    new_dog.name =  row[1]
+    new_dog.breed = row[2]
+    new_dog 
+  end
+  
+end
